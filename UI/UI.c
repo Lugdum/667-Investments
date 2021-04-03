7f9a3eca7cacb7a7ea4adcd1c97a3f4d788e3bf5
Joris <bouboulover17@gmail.com>
2021-04-03 23:32:15 +0200

    value button

--- UI/UI.c
+++ UI/UI.c
@@ -12,6 +12,7 @@
 #include <err.h>
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
+//#include "../ValueGrabber/fetcher.c"
 
 GtkWidget *window;//we set global variables
 
@@ -28,6 +29,8 @@ GtkWidget *value_entry;
 GtkWidget *btc_possess;
 GtkWidget *eth_possess;
 GtkWidget *doge_possess;
+
+GtkWidget *value_label;
 
 GtkBuilder *builder;
 
@@ -61,10 +64,15 @@ int main()
  eth_possess= GTK_WIDGET(gtk_builder_get_object(builder,"eth_possess"));
 
  doge_possess= GTK_WIDGET(gtk_builder_get_object(builder,"doge_possess"));
+
+ value_label= GTK_WIDGET(gtk_builder_get_object(builder,"value_label"));
+ // permet de directement montrer la valeur du BTC
+ gtk_label_set_text(GTK_LABEL(value_label), (gchar*)"1 BTC = xxx$");
  
  gtk_widget_show(window);
 
  gtk_main();
+
 
  return EXIT_SUCCESS;
 }
@@ -75,11 +83,20 @@ void on_quit_button_clicked()
 }
 
 
-void on_btc_graph_button_toggled();
+void on_btc_graph_button_toggled()
+{
+  gtk_label_set_text(GTK_LABEL(value_label), (gchar*)"1 BTC = xxx$");
+}
 
-void on_eth_graph_button_toggled();
+void on_eth_graph_button_toggled()
+{
+  gtk_label_set_text(GTK_LABEL(value_label), (gchar*)"1 ETH = xxx$");
+}  
 
-void on_doge_graph_button_toggled();
+void on_doge_graph_button_toggled()
+{
+  gtk_label_set_text(GTK_LABEL(value_label), (gchar*)"1 DOGE = xxx$");
+} 
 
 void on_buy_button_clicked();
 
@@ -92,6 +109,8 @@ void on_btc_possess();
 void on_eth_possess();
 
 void on_doge_possess();
+
+void on_value_label();
 
 
 
